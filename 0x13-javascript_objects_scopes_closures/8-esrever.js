4cbb9ea08951cd8b653808513f2e913d0dab96d6
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-11-29 16:47:18 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -6,6 +6,6 @@ exports.esrever =  function (list) {
     let rlist = [];
     while (list.length > 0) {
         rlist.push(list.pop());
-    }
-    return rlist;
+        }
+        return rlist;
 };
