c9e9eb385b7593bc81cd094013272f9475581a6f
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-12-03 09:43:17 +0300

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 // function that return the number of occurrences in a list
 
-exports.nbOccurences = function (list, searchElement) {
-	return list.filter((value) => (value === searchElement)).length;
-};
+exports.nbOccurences = function (list, searchElement){
+    let i;
+    let j = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === searchElement) {
+           j = j + 1;
+        }
+    }
+    return (j);
+}
