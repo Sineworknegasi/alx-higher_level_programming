2d4651b9ddc5cc81d373bdce7eb0e87d522bf1af
Sineworknegasi <Sineworknegasi38@gmail.com>
2023-01-17 12:23:40 +0300

    web-scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
+// A script that writes a string to a file
+
 const fs = require('fs');
-const args = process.argv;
-fs.writeFile(args[2],args[3], (err) => {
-	if (err) console.log(err);
+const file = process.argv[2];
+const write_ = process.argv[3];
+
+fs.writeFile(file, write_, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
 });
